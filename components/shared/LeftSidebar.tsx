4acc25471d55e9e1659846bf06ee53c3503e3e0a
MohitG780 <mohitgupta000780@gmail.com>
2025-07-16 17:08:36 +0530

    search updated

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { sidebarLinks } from "@/constants";
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
+import { SignedIn, SignOutButton,useAuth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 function LeftSidebar() {
   const pathname = usePathname();
-
+ const {userId}=useAuth();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
@@ -16,6 +16,8 @@ function LeftSidebar() {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
+
+            if(link.route==='/profile') link.route=`${link.route}/${userId}`
 
           return (
             <Link
