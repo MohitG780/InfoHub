81f926579ab2b79eb38a489e6dfc592000d496df
MohitG780 <mohitgupta000780@gmail.com>
2025-07-18 23:23:29 +0530

    updated

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,10 +1,11 @@
-import getActivity, {fetchUser } from "@/lib/actions/user.actions";
-import { currentUser } from "@clerk/nextjs/server";
 import Image from "next/image";
 import Link from "next/link";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-const Page = async () => {
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+
+async function Page() {
   const user = await currentUser();
   if (!user) return null;
 
@@ -14,35 +15,38 @@ const Page = async () => {
   const activity = await getActivity(userInfo._id);
 
   return (
-    <section>
-      <h1 className="head-text mb-10">Activity</h1>
+    <>
+      <h1 className='head-text'>Activity</h1>
 
-      <section className="mt-10 flex flex-col gap-5">
+      <section className='mt-10 flex flex-col gap-5'>
         {activity.length > 0 ? (
           <>
-            {activity.map((item: any) => (
-              <Link key={item._id} href={`/thread/${item.parentId}`}>
-                <article className="activity-card">
+            {activity.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className='activity-card'>
                   <Image
-                    src={item.author.image}
-                    alt="Profile Picture"
+                    src={activity.author.image}
+                    alt='user_logo'
                     width={20}
                     height={20}
-                    className="rounded-full object-cover"
+                    className='rounded-full object-cover'
                   />
-                  <p className="text-light-1">
-                    {item.author.name} replied to your thread
+                  <p className='!text-small-regular text-light-1'>
+                    <span className='mr-1 text-primary-500'>
+                      {activity.author.name}
+                    </span>{" "}
+                    replied to your thread
                   </p>
                 </article>
               </Link>
             ))}
           </>
         ) : (
-          <p>No activity yet</p>
+          <p className='!text-base-regular text-light-3'>No activity yet</p>
         )}
       </section>
-    </section>
+    </>
   );
-};
+}
 
 export default Page;
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -130,25 +130,30 @@ throw new Error(`Failed to fetch users :${error.message}`)
 }
 }
 
-export default async function getActivity(userId:string){
-    try{
-        connectToDB();
-  const userThreads=await Thread.find({author:userId});
-  
-  ['This is great','This is bad','This is terrible','ok','bad']
-  const childThreadsIds=userThreads.reduce((acc,userThread)=>{
-    return acc.concat(userThread.children)
-  })
+export async function getActivity(userId: string) {
+  try {
+    connectToDB();
 
-  const replies=await Thread.find({
-    _id:{$in:childThreadsIds},
-    author:{$ne:userId}
-  }).populate({
-    path:'author',model:User,
-    select:'name image _id'
-  })
-  return replies;
-    }catch(error:any){
-        throw new Error(`Failed to fetch users :${error.message}`)
-    }
+  
+    const userThreads = await Thread.find({ author: userId });
+
+    
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId }, 
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error) {
+    console.error("Error fetching replies: ", error);
+    throw error;
+  }
 }
